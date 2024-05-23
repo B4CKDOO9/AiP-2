4ca4f2edd6bbb4ae83f6c73d1ff2ee32aa3d8260
The Mentor <smolecprofesional@gmail.com>
2024-05-24 01:17:14 +0200

    Updated code
    Hopefully it works!

--- DZ_12_5/UcenikSort.cpp
+++ DZ_12_5/UcenikSort.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
+struct Ucenik
+{
+    char imePrezime[50];
+    float prosjek;
+};
+
+bool cmp(Ucenik &a, Ucenik &b) 
+{
+    return a.prosjek > b.prosjek;
+}
+
 int main()
 {
-
+    struct Ucenik ucenici[100];
+    int brUcenika = 0;
+    fstream datotekica("podatci.bin", ios::binary | ios::in);
+    while (datotekica.read((char*)&ucenici[brUcenika], sizeof(Ucenik)))
+    {
+        cout << ucenici[brUcenika].imePrezime << " " << ucenici[brUcenika].prosjek << endl;
+        brUcenika++;
+    }
+    datotekica.close();
+    int n;
+    cin >> n;
+    for (int i = 0; i < 0; i++)
+    {
+        cin.getline(ucenici[brUcenika + i].imePrezime, 50);
+        cin >> ucenici[brUcenika + i].prosjek;
+    }
+    sort(ucenici,ucenici + brUcenika + n,cmp);
+    datotekica.open("podatci.bin",ios::binary | ios::out | ios::trunc);
+    datotekica.write((char*)ucenici,sizeof(Ucenik)*(brUcenika + n));
+    datotekica.close();
+    return 0;
 }
